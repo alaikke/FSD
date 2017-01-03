f40c545736320ec36ce08d6e4d04a769c920c106
alaikke <deejayalaikke@gmail.com>
2017-01-03 01:41:24 -0200

    gulp sass funcionando

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
+'use strict';
+ 
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
-
-// task para o sass
+var sass = require('gulp-sass');
+ 
 gulp.task('sass', function () {
-    return gulp.sass('sass/*.sass').pipe(gulp.dest('css'))
+  return gulp.src('./sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
-
-//task default
-gulp.task('default', function(){
-	console.log ('olá');
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
