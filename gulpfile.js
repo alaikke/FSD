67fe37dd20e6607a2d47991be2958fb1aece51a5
alaikke <deejayalaikke@gmail.com>
2017-01-03 04:18:54 -0200

    lets start

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,18 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
  
  //task sass
+
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.sass')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
  
+ //task watch
+
 gulp.task('watch', function () {
   gulp.watch('./sass/**/*.sass', ['sass']);
 });
+
+//task default
+gulp.task('default',['sass','watch']);
