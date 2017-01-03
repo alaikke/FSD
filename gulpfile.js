bc3d4f4aba5bd1b7352ae3f278654fdb39cf01dd
alaikke <deejayalaikke@gmail.com>
2017-01-03 02:01:31 -0200

    gulp sass + watch funcionando

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,15 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
  
+ //task sass
 gulp.task('sass', function () {
-  return gulp.src('./sass/*.sass')
+  return gulp.src('./sass/**/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
  
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.sass', ['sass']);
 });
